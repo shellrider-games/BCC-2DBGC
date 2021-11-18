82e90e3b5d531c9b133f27b49c90cb80510f0ca0
Becker Georg <cc211007@fhstp.ac.at>
2021-11-18 19:46:00 +0100

    Add file for rectangles

--- 20211118/script.js
+++ 20211118/script.js
@@ -2,18 +2,3 @@ const canvas = document.querySelector('#gameCanvas');
 const ctx = canvas.getContext('2d');
 
 
-ctx.beginPath();
-ctx.moveTo(200,50);
-ctx.lineTo(200,200);
-ctx.lineTo(400,200);
-ctx.closePath();
-ctx.lineWidth = 16;
-ctx.stroke();
-
-ctx.beginPath()
-ctx.moveTo(50, 300);
-ctx.lineTo(50, 400);
-ctx.moveTo(100,300);
-ctx.lineTo(100, 400);
-ctx.lineWidth = 10;
-ctx.stroke();
